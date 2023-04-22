1fef0ca23296276f95f126ccaa8a917c6f707238
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-22 16:49:59 +0500

    Reformate code

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,34 +1,36 @@
 package ru.netology.stats;
+
 public class StatsService {
 
-    public int sumSales(int[] sales){
+    public int sumSales(int[] sales) {
         int sum = 0;
 
-        for (int i = 0; i < sales.length; i++){
+        for (int i = 0; i < sales.length; i++) {
             sum = sum + sales[i];
         }
         return sum;
     }
 
-    public double averageAmountSales(int[] sales){
+    public double averageAmountSales(int[] sales) {
         double averageAmount = 0;
         int sum = 0;
-        for (int i = 0; i < sales.length; i++){
+        for (int i = 0; i < sales.length; i++) {
             sum = sum + sales[i];
         }
         return averageAmount = sum / sales.length;
     }
 
-    public int maxSales(int[] sales){
+    public int maxSales(int[] sales) {
         int maxMonth = 0;
 
-        for (int i = 0; i < sales.length; i++){
-            if (sales[i] >= sales[maxMonth]){
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
                 maxMonth = i;
             }
         }
         return maxMonth + 1;
     }
+
     public int minSales(int[] sales) {
         int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
 
@@ -41,24 +43,24 @@ public class StatsService {
         return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
     }
 
-    public int belowAverageSales(int[] sales){
+    public int belowAverageSales(int[] sales) {
         int belowAverageMonth = 0;
         double averageAmount = averageAmountSales(sales);
 
-        for (int i = 0; i < sales.length; i++){
-            if (sales[i] < averageAmount){
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < averageAmount) {
                 belowAverageMonth = belowAverageMonth + 1;
             }
         }
         return belowAverageMonth;
     }
 
-    public int aboveAverageSales(int[] sales){
+    public int aboveAverageSales(int[] sales) {
         int aboveAverageMonth = 0;
         double averageAmount = averageAmountSales(sales);
 
-        for (int i = 0; i < sales.length; i++){
-            if (sales[i] > averageAmount){
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > averageAmount) {
                 aboveAverageMonth = aboveAverageMonth + 1;
             }
         }
